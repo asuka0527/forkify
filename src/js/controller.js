347d942f64c5da5a38ca19ca9fcf5c8150e64427
asuka0527 <junizzanakajima@gmail.com>
2020-11-30 18:30:00 +0900

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -156,10 +156,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 // controlSearchResults();
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -169,7 +165,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
